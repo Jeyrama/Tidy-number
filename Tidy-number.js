00ff77a8aedb3008cfc77f6a244bf58a97927591
Jey <126204426+Jeyrama@users.noreply.github.com>
2023-07-07 14:14:45 -0700

    Create Tidy-number.js

---
+++ Tidy-number.js
@@ -0,0 +1,17 @@
+/*
+A Tidy number is a number whose digits are in non-decreasing order.
+
+Task:
+  Given a number, find if it is tidy or not.
+
+Notes:
+  Number passed is always Positive.
+  Return the result as a Boolean.
+
+Examples:
+  tidyNumber (12) ==> return (true)
+
+  tidyNumber (1024) ==> return (false)
+
+  tidyNumber (13579) ==> return (true)
+*/
