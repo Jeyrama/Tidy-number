cdd526640ef10063fffab98b8ad6d2b2512171b3
Jey <126204426+Jeyrama@users.noreply.github.com>
2023-07-07 14:18:35 -0700

    Update Tidy-number.js

--- Tidy-number.js
+++ Tidy-number.js
@@ -18,3 +18,11 @@ Examples:
 
 
 // Solution
+
+const tidyNumber = n => {
+  let s = n.toString();
+  for (let i = 0; i < s.length-1; i++) {
+    if (s[i] > s[i+1]) return false;
+  }
+  return true;
+}
